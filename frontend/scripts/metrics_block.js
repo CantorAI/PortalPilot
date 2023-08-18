2d11e3dd730da45daad989cdac7abf7078be9799
Shawn <shawn@cantorai.com>
2023-08-18 02:46:05 -0700

    metrics

--- frontend/scripts/metrics_block.js
+++ frontend/scripts/metrics_block.js
@@ -1,20 +1,41 @@
 const DATA_POINTS = 120; // 2 minutes of data at 1s interval
 const BUFFER_SIZE = 5 * DATA_POINTS;
-const colors = {
-    "CPU": "blue",
-    "CantorCPU": "yellow",
-    "ThreadCount":"green",
-    "Memory": "red"
-};
-
 class Metrics {
     constructor(updatePeriod, timeWindow) {
         this.updatePeriod = updatePeriod;
         this.timeWindow = timeWindow;
         this.map = new Map();
+        this.colors = new Map();
     }
-    addMetric(name) {
+    getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+    getLatestTime() {
+        if (this.map.size == 0) {
+            return 0;
+        }
+        const firstKey = this.map.keys().next().value;
+        if (this.map.get(firstKey).length > 0) {
+            return this.map.get(firstKey).slice(-1)[0].timestamp;
+        }
+        else {
+            return 0;
+        }
+    }
+    Colors(name) {
+        return this.colors.get(name);
+    }
+    addMetric(name,color) {
         this.map.set(name, []);
+        if (color == undefined) {
+            color = this.getRandomColor();
+        }
+        this.colors.set(name, color);
     }
 }
 
@@ -39,6 +60,33 @@ function fetchData_fake(minVal, maxVal) {
 
 class MetricsBlock extends HTMLElement {
     static get observedAttributes() { return ['src']; }
+
+    attributeChangedCallback(attrName, oldVal, newVal) {
+        this[attrName] = newVal;
+        if (attrName == "src") {
+            this.parseMetrics(newVal);
+        }
+    }
+    //src format: "{[objectKey.]Metrics_Name[:color][,]}*"
+    //test
+    //const input = "obj1.metric1:blue,obj2.metric2:red,metric3";
+    //console.log(parseMetrics(input));
+    parseMetrics(input) {
+        const regex = /(([^.,:]+)\.)?([^.,:]+)(:([^,]+))?/g;
+
+        const results = [];
+        let match;
+
+        while (match = regex.exec(input)) {
+            let objectKey = match[2] || undefined;
+            let metricName = match[3] || undefined;
+            let color = match[5] || undefined;
+            this.metrics.addMetric(metricName, color);
+        }
+
+        return results;
+    }
+
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
@@ -51,10 +99,10 @@ class MetricsBlock extends HTMLElement {
         this.svg = null;
 
         this.metrics = new Metrics(1000, DATA_POINTS * 1000);//use 1000 from seconds to to ms
-        this.metrics.addMetric("CPU");
-        this.metrics.addMetric("Memory");
-        this.metrics.addMetric("ThreadCount");
-        this.metrics.addMetric("CantorCPU");
+        //this.metrics.addMetric("CPU");
+        //this.metrics.addMetric("Memory");
+        //this.metrics.addMetric("ThreadCount");
+        //this.metrics.addMetric("CantorCPU");
 
         this.addStyles(`
             svg {
@@ -67,11 +115,25 @@ class MetricsBlock extends HTMLElement {
     }
     connectedCallback() {
         let self = this;
-
-        this.svg = d3.select(this.shadowRoot).append("svg")
+        const container = document.createElement('div');
+        container.style.position = 'relative'; 
+        this.svg = d3.select(container).append("svg")
             .attr("width", this.width)
             .attr("height", this.height);
         this.svg.append("g").classed("graph-group", true);
+
+        // Create and append a button
+        const button = document.createElement('button');
+        button.textContent = 'Reset';
+        button.style.position = 'absolute';  // Position the button as absolute
+        button.style.top = '10px';  // Adjust as needed for your desired fixed coordinate
+        button.style.left = '10px';
+        button.onclick = () => {
+            self.resetView();
+        };
+
+        container.appendChild(button);
+        this.shadowRoot.appendChild(container);
 
         this.svg.on("wheel", function (event) {
             event.preventDefault();
@@ -165,10 +227,7 @@ class MetricsBlock extends HTMLElement {
         let now = new Date();
         let timeSpan = thisBlock.metrics.timeWindow;
 
-        let latestTime = 0;
-        if (thisBlock.metrics.map.get("CPU").length > 0) {
-            latestTime = thisBlock.metrics.map.get("CPU").slice(-1)[0].timestamp;
-        }
+        let latestTime = thisBlock.metrics.getLatestTime();
         let earliestTime = latestTime - BUFFER_SIZE * thisBlock.metrics.updatePeriod;
 
         let shiftedEndTime = new Date(latestTime - thisBlock.currentOffset.x);
@@ -191,20 +250,110 @@ class MetricsBlock extends HTMLElement {
             .attr("transform", "translate(50,0)")
             .call(d3.axisLeft(thisBlock.scaleY));
 
+        let hideTooltipTimeout;
         // Drawing curves
         thisBlock.metrics.map.forEach((data, metricName) => {
+            let checkName = thisBlock.metrics.Colors(metricName);
             thisBlock.svg.append("path")
                 .datum(data)
+                .attr("name", metricName)
                 .attr("fill", "none")
-                .attr("stroke", colors[metricName])
+                .attr("stroke", thisBlock.metrics.Colors(metricName))
                 .attr("stroke-width", 2)
                 .attr("d", d3.line()
                     .x(d => thisBlock.scaleX(d.timestamp))
                     .y(d => thisBlock.scaleY(d.value))
                 )
-                .attr("clip-path", "url(#clip)");
-        });
+                .attr("clip-path", "url(#clip)")
+                .on("mouseover", function (event,d) {
+                    const pathNode = this;
+                    const pathData = d;
+                    const [mouseX, mouseY] = d3.pointer(event);
+                    const threshold = 100;
+                    let closestPoint = { x: 0, y: 0 };
+                    let closestDistance = Infinity;
+                    let closestDataIndex = null;
+                    let interpolatedY = 0;
+                    // Iterate over data points, which represent the segments
+                    for (let i = 0; i < data.length - 1; i++) {
+                        const start = pathNode.getPointAtLength(i * pathNode.getTotalLength() / (data.length - 1));
+                        const end = pathNode.getPointAtLength((i + 1) * pathNode.getTotalLength() / (data.length - 1));
 
+                        // Find the mid-point of the segment
+                        const midX = (start.x + end.x) / 2;
+                        const midY = (start.y + end.y) / 2;
+
+                        const distance = Math.sqrt(Math.pow(mouseX - midX, 2) + Math.pow(mouseY - midY, 2));
+
+                        if (distance < closestDistance) {
+                            closestDistance = distance;
+                            closestDataIndex = i;
+                            // Linear interpolation to find the Y value at mouseX
+                            interpolatedY = (mouseX - start.x) / (end.x - start.x);
+                            const interpolatedX = start.x + interpolatedY * (end.x - start.x);
+                            const interpolatedSegmentY = start.y + interpolatedY * (end.y - start.y);
+                            closestPoint.x = interpolatedX;
+                            closestPoint.y = interpolatedSegmentY;
+                        }
+                    }
+                    if (closestDistance < threshold) {
+                        const name = d3.select(this).attr("name");
+                        const segmentDataStart = pathData[closestDataIndex];
+                        const segmentDataEnd = pathData[closestDataIndex + 1];
+                        const val = segmentDataStart.value + (segmentDataEnd.value - segmentDataStart.value) * interpolatedY;
+                        tooltip
+                            .attr("x", mouseX)
+                            .attr("y", mouseY)
+                            .text(`${name}: ${val.toFixed(2)}`)
+                            .attr("visibility", "visible");
+                        circle_mouseover
+                            .attr("cx", closestPoint.x)
+                            .attr("cy", closestPoint.y)
+                            .attr("visibility", "visible");
+                        const height = +svg.attr("height");
+                        guideline
+                            .attr("x1", closestPoint.x)
+                            .attr("y1", thisBlock.scaleY(100))
+                            .attr("x2", closestPoint.x)
+                            .attr("y2", thisBlock.scaleY(0))
+                            .attr("visibility", "visible");
+                    }
+                    else {
+                        hideTooltipTimeout = setTimeout(() => {
+                            tooltip.attr("visibility", "hidden");
+                            circle_mouseover.attr("visibility", "hidden");
+                            guideline.attr("visibility", "hidden"); 
+                        }, 500);
+                    }
+                })
+        });
+        // Assuming you've already selected your SVG
+        const svg = thisBlock.svg;
+        const tooltip = svg.append("text")
+            .attr("visibility", "hidden")
+            .attr("text-anchor", "middle")
+            .attr("dy", "-1em") // Position above the mouse pointer
+            .style("font-size", "12px")
+            .style("background-color", "white");
+        const circle_mouseover = svg.append("circle")
+            .attr("r", 3)  // Radius of circle
+            .attr("fill", "none")  // No fill
+            .attr("stroke", "red") // Blue stroke color
+            .attr("stroke-width", 1) // Width of the stroke, adjust as needed
+            .attr("visibility", "hidden");
+        const guideline = svg.append("line")
+            .attr("stroke", "blue")
+            .attr("stroke-dasharray", "2,2") 
+            .attr("stroke-width", 1)
+            .attr("visibility", "hidden");
+        // Hide tooltip on mouse out of the SVG
+        svg.on("mouseout", function () {
+            hideTooltipTimeout = setTimeout(() => {
+                tooltip.attr("visibility", "hidden");
+                circle_mouseover.attr("visibility", "hidden");
+                guideline.attr("visibility", "hidden"); 
+            }, 500);
+        });
         // Drawing legend
         let legend = thisBlock.svg.selectAll(".legend")
             .data(Array.from(thisBlock.metrics.map.keys()))
@@ -214,14 +363,14 @@ class MetricsBlock extends HTMLElement {
             .attr("transform", (d, i) => "translate(0," + (20 * i) + ")");
 
         legend.append("rect")
-            .attr("x", 510)
+            .attr("x", 480)
             .attr("y", 10)
             .attr("width", 18)
             .attr("height", 18)
-            .style("fill", d => colors[d]);
+            .style("fill", d => thisBlock.metrics.Colors(d));
 
         legend.append("text")
-            .attr("x", 530)
+            .attr("x", 500)
             .attr("y", 20)
             .attr("dy", ".35em")
             .style("text-anchor", "start")
